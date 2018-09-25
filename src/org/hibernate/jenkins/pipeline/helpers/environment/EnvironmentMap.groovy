a5d042da5468d916be11140a26402eaf0fc71b9a
Yoann Rodière <yoann@hibernate.org>
2018-09-25 12:54:50 +0200

    Initial implementation of the helpers

---
+++ src/org/hibernate/jenkins/pipeline/helpers/environment/EnvironmentMap.groovy
@@ -0,0 +1,35 @@
+/*
+ * Hibernate Helpers for Jenkins pipelines
+ *
+ * License: Apache License, version 2 or later.
+ * See the LICENSE.txt file in the root directory or <https://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.jenkins.pipeline.helpers.environment
+
+class EnvironmentMap {
+	@Delegate
+	private final Map<String, List<?>> all
+
+	final Map<String, ?> defaults
+
+	EnvironmentMap(Map<String, List<?>> all) {
+		this(all, {it -> it.isDefault()})
+	}
+
+	EnvironmentMap(Map<String, List<?>> all, Closure defaultPicker) {
+		this.all = all
+		// Defaults are set before any user modification (environment removals); this is on purpose.
+		all.each { key, envList ->
+			defaults.put(key, envList.find(defaultPicker))
+		}
+	}
+
+	@Override
+	String toString() {
+		return all.values().flatten().join(', ')
+	}
+
+	boolean isEmpty() {
+		all.isEmpty() || all.every { key, envList -> envList.isEmpty() }
+	}
+}
---
+++ src/org/hibernate/jenkins/pipeline/helpers/job/configuration/MavenConfiguration.groovy
@@ -0,0 +1,58 @@
+/*
+ * Hibernate Helpers for Jenkins pipelines
+ *
+ * License: Apache License, version 2 or later.
+ * See the LICENSE.txt file in the root directory or <https://www.apache.org/licenses/LICENSE-2.0>.
+ */
+package org.hibernate.jenkins.pipeline.helpers.job.configuration
+
+import groovy.transform.PackageScope
+import groovy.transform.PackageScopeTarget
+import groovy.transform.TypeChecked
+import groovy.transform.TypeCheckingMode
+
+@TypeChecked
+@PackageScope([PackageScopeTarget.CONSTRUCTORS, PackageScopeTarget.FIELDS, PackageScopeTarget.METHODS])
+class MavenConfiguration {
+	private String localRepositoryPath
+	private String defaultTool
+	private List<String> producedArtifactPatterns = []
+
+	@TypeChecked(TypeCheckingMode.SKIP)
+	MavenConfiguration(def script) {
+		localRepositoryPath = "$script.env.WORKSPACE/.repository"
+	}
+
+	public String getLocalRepositoryPath() {
+		return localRepositoryPath
+	}
+
+	public String getDefaultTool() {
+		defaultTool
+	}
+
+	public getProducedArtifactPatterns() {
+		return producedArtifactPatterns
+	}
+
+	void complete() {
+		if (!defaultTool) {
+			throw new IllegalStateException("Missing default tool for Maven")
+		}
+	}
+
+	@TypeChecked
+	@PackageScope([PackageScopeTarget.CONSTRUCTORS])
+	public class DSLElement {
+		DSLElement() {
+		}
+
+		void defaultTool(String toolName) {
+			defaultTool = toolName
+		}
+
+		void producedArtifactPattern(String pattern) {
+			producedArtifactPatterns.add(pattern)
+		}
+	}
+}
